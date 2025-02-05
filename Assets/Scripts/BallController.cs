60e3932df07396780a339435ecbfcfa1a8121e03
lexilouds <lexiloudiadis@gmail.com>
2025-02-05 11:22:31 -0800

    Basic bowling and pins setup

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
    [SerializeField] private float force = 1f;
    [SerializeField] private InputManager inputManager;
    private Rigidbody ballRB;
+   private bool isBallLaunched;
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
@@ -19,6 +20,8 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall() 
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
