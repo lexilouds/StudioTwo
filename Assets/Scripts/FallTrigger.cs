7707a197afe758a5db81dcd8bc6599beff78b72b
lexilouds <lexiloudiadis@gmail.com>
2025-02-09 15:51:52 -0800

    Added pin falling logic

--- Assets/Scripts/FallTrigger.cs
+++ Assets/Scripts/FallTrigger.cs
@@ -12,15 +12,4 @@ public class FallTrigger : MonoBehaviour
         OnPillFall?.Invoke();
         Debug.Log($"{gameObject.name} is fallen");
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
